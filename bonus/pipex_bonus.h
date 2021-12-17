12944ed991d881d0d71db1938e00f977b54da9ea
Anna JEARUTH <ajearuth@student.42.fr>
2021-12-17 16:30:12 +0100

    bonus inc

---
+++ bonus/pipex_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajearuth <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 15:32:49 by ajearuth          #+#    #+#             */
+/*   Updated: 2021/12/17 15:36:06 by ajearuth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h> 
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+#endif
